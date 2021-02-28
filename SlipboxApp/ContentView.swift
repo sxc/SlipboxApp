70d33997db3f58cf63f78b73c25c59ccabc444b4
sxc <qiuyequ@icloud.com>
2021-02-28 22:17:57 +0800

    Initial Commit

---
+++ SlipboxApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SlipboxApp
+//
+//  Created by Xiaochun Shen on 2021/2/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
